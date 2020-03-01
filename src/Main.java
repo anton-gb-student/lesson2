23ad20ac6b355704c6e26d66bcb6a228c6e900c1
Anton <dyacki@gmail.com>
2020-03-01 22:55:10 +0500

    Test for 3 situations: 1. OK 2. Exception 1 3. Exception 2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,40 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[][] arr = {
-                {"10","10","10","10"},
-                {"10","10","10","10"},
-                {"10","10.0","10","10"},
-                {"10","10","10","10"}
+        String[][][] arr = {
+                {
+                        {"10", "10", "10", "10"},
+                        {"10", "10", "10", "10"},
+                        {"10", "10", "10", "10"},
+                        {"10", "10", "10", "10"}
+                },
+                {
+                        {"10", "10", "10", "10"},
+                        {"10", "10", "10", "10"},
+                        {"10", "10", "10"},
+                        {"10", "10", "10", "10"}
+                },
+                {
+                        {"10", "10", "10", "10"},
+                        {"10", "10", "10", "10"},
+                        {"10", "10.0", "10", "10"},
+                        {"10", "10", "10", "10"}
+                }
         };
-        ExceptionTest excTest = new ExceptionTest();
-        try {
-            excTest.sum(arr);
-            System.out.println(excTest.getSum());
 
-        } catch (MyArrayDataException e) {
-            System.out.println(e);
-        } catch (MyArraySizeException e) {
-            System.out.println(e);
+
+        ExceptionTest excTest = new ExceptionTest();
+        for (int i = 0; i < 3; i++) {
+            try {
+                excTest.sum(arr[i]);
+                System.out.println(excTest.getSum());
+
+            } catch (MyArrayDataException v) {
+                System.out.println(v);
+            } catch (MyArraySizeException u) {
+                System.out.println(u);
+            }
+
         }
     }
 }
